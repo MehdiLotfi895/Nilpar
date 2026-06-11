93993a82030e8fba1616b1ddd9ac028938618145
mahdi lotfi <mahdilotfi249@gmail.com>
2026-06-10 17:46:14 -0800

    change slider_image mobile

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,10 +23,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv(
-    'ALLOWED_HOSTS',
-    '127.0.0.1,localhost'
-).split(',')
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".ngrok-free.dev",
+    "rumor-roving-goldfish.ngrok-free.dev",
+]
 
 # =========================
 # INSTALLED APPS
